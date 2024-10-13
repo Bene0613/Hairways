d46a5f1b435af1f87e28d10eac0c44b7dd0af56a
Bene0613 <mbenetshimanga.gmail.com>
2024-10-13 13:09:48 +0200

    Updated login.php with new authentication logic

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
-<!DOCTYPE html>
+<?php
+//$conn= new mysqli('localhost', 'root', '', 'Hairways');
+    //select * from products and loop
+
+   // $sql = "SELECT * FROM product";
+    //$result = $conn->query($sql);
+    //$product = $result->fetch_all(MYSQLI_ASSOC);
+
+    $conn = new PDO ('mysql:dbname=Hairways;host=localhost',"root", "");
+
+    // select * frpm products and fetch as array
+    $statement = $conn -> prepare('SELECT * FROM product');
+    $statement->execute();
+    $product = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,6 +21,11 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Take care of your hair with Hairways </h1>
+    <h1>Ways to love your hair with Hairways </h1>
+    <?php foreach($product as $prod): ?>
+    <article>
+        <h2> <?php echo $prod ['name'];?></h2>
+    </article>
+    <?php endforeach; ?>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,11 +1,55 @@
-<!DOCTYPE html>
+<?php
+    function canIn($username, $password) {
+        if ($username === "jenaam@shop.com" && $password ==="12345isnotsecure") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if (!empty($_POST)) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if(canIn($username, $password)) {
+            session_start();
+            $_SESSION["username"] = $username;
+            header("Location: index.php");
+            exit;
+        } else {
+            $error = true;
+        }
+
+    }
+    
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div class="wrap">
+        <div class ="euhm">
+            <form action="" method="POST">
+            <h2>Log in</h2>
+                <div class= "text">
+                    <input type ="text" name="username" placeholder= "Username">
+                </div>
+                <div class= "text">
+                    <input type ="password" name="password" placeholder= "Password">
+                </div>
+                <button type = "submit">Log in</button>
+                <div class="alert <?php echo $error ? '': 'hidden'; ?>">
+                ops, we couldn't log you in. Let’s try again!
+                </div>
+                <div class="more">
+                    <p>Don't have an account? <a href="#">Sign Up </a></p>
+                </div>
+            </form>
+        </div>
+</div>
 </body>
 </html>
