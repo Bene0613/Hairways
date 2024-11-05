7b56a54311d65eea74a4a4019f1f2631559c6c8b
Bene0613 <mbenetshimanga.gmail.com>
2024-11-05 11:29:57 +0100

    aangepast

--- product.php
+++ product.php
@@ -1,14 +1,12 @@
 <?php
+    $conn = new mysqli("localhost","root","","Hairways");
+    $products = $conn->query("SELECT name, price, image_url FROM products");
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage Hairways</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Products</title>
     <style>
 
         body{
@@ -43,47 +41,44 @@
             width: 50px;
         }
 
-        .banner {
-            position: relative;
-            color: white;
+        .articles {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-template-rows: auto auto;
+            gap: 20px;
+            margin: 0 auto;
         }
 
-        .banner img {
-            width: 100%;
-            height:auto;
+        .prd {
+            text-align: center;
+            border: 1px solid #D8BFD8;
+            padding: 10px;
+            border-radius: 8px;
         }
 
-        .ban_text {
-            position: absolute;
-            top: 50%;
-            left: 20px;
-            padding: 20px;
-            border-radius: 10px;
-            transform: translateY(-50%)
+        .prd:hover {
+            border: 3px solid #D8BFD8;
+            cursor: pointer;
         }
 
-        .ban_text h1 {
-            font-size: 70px;
+        .prd img {
+            max-width: 100%;
+            height: auto;
+            border-radius: 5px
         }
 
-        button {
-            background-color: #9F4FA3;
-            color: white;
-            padding: 10px 20px;
-            border-radius: 5px;
-            text-decoration: none;
-        }
-
-        button:hover {
-            background-color:#DB92B8;
+        .prd h2 {
+            color: #301934;
+            font-size: 1.2em;
+            margin: 10px 0;
         }
     </style>
 </head>
 <body>
-    <nav class="nbar">
+<nav class="nbar">
         <div class="one">
            <ul>
-                <li><a href="product.php">SHOP</a></li>
+                <li><a href="shop.html">SHOP</a></li>
                 <li><a href="bestseller.html">BESTSELLERS</a></li>
                 <li><a href="sets.html">SETS</a></li>
                 <li><a href="about.html">ABOUT US</a></li>
@@ -99,13 +94,16 @@
             </ul>
         </div>
     </nav>
-    <div class="banner">
-        <img src="images/1x/banner.png" alt="a banner">
-        <div class="ban_text">
-            <h1>HairWays</h1>
-            <h2>Ways to take care of your hair.</h2>
-            <button><a href="shop.html">Shop now</a></button>
-        </div>
+
+    <h1>Our product list</h1>
+    <div class="articles">
+        <?php foreach ($products as $prod): ?>
+           <div class="prd">
+           <img src="<?php echo $prod['image_url']; ?>" alt="<?php echo $prod['name']; ?>">
+            <h2><?php echo $prod['name']; ?></h2>
+            <h3><?php echo $prod['price']; ?></h3>
+           </div>
+        <?php endforeach;?>
     </div>
 </body>
 </html>
